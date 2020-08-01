97ada1bcdf409470fa920057d510fe74f0a3a24a
rx78se <rairao@gmail.com>
2020-08-01 14:56:30 +0900

    p.175까지 진행.

---
+++ src/main/java/com/rairao/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,12 @@
+package com.rairao.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id desc")
+    List<Posts> findAllDesc();
+}
