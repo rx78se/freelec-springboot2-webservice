da673869004daffc7b83a4a3b98940ec628522b5
rx78se <rairao@gmail.com>
2020-11-01 00:04:29 +0900

    renew#5

--- scripts/health.sh
+++ scripts/health.sh
@@ -2,6 +2,7 @@
 
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 source ${ABSDIR}/switch.sh
 
 IDLE_PORT=$(find_idle_port)
@@ -14,7 +15,7 @@ sleep 10
 for RETRY_COUNT in {1..10}
 do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -1)
+  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= ("real" 문자열이 있는지 검증)
